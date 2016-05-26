811a054558c6361a18cde6c54137f6427b318164
Jose Junior <jose.junior@gmail.com>
2016-05-26 01:19:25 +0000

    Uses a different servo strategy

--- fish_food.py
+++ fish_food.py
@@ -5,9 +5,35 @@ import time
 food_pin = 17
 turns = 2
 
+# positions
+center = 1500
+cw = 500
+ccw = 2500
+
 # initializing
 pi = pigpio.pi()
-pi.set_servo_pulsewidth(food_pin, 1000)
-time.sleep(0.91)
+
+def shake_it(pin, shakes = 2):
+    # center the stem
+    print "initial center"
+    pi.set_servo_pulsewidth(pin, center)
+    time.sleep(2)
+
+    print "ccw"
+    pi.set_servo_pulsewidth(pin, ccw)
+    time.sleep(0.5)
+    print "cw"
+    pi.set_servo_pulsewidth(pin, cw)
+    time.sleep(0.5)
+
+    pi.set_servo_pulsewidth(pin, center)
+    time.sleep(3)
+    return
+
+# shake it
+print "Shaking"
+shake_it(food_pin)
+
+
 pi.set_servo_pulsewidth(food_pin, 0)
 
